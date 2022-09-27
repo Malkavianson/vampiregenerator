7afb6571bed0e670f837276661032cd09754796f
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-09-27 10:07:46 -0300

    fix: protect routes

--- src/router.tsx
+++ src/router.tsx
@@ -1,4 +1,5 @@
 import Home from "./pages/Home";
+import { useState } from "react";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
@@ -6,29 +7,40 @@ import Generate from "./pages/Generate";
 import { Route, Routes } from "react-router-dom";
 
 const Router = (): JSX.Element => {
-	return (
-		<Routes>
-			<Route
-				path="/"
-				element={<Home />}
-			/>
-			<Route
-				path="/profile"
-				element={<Profile />}
-			/>
-			<Route
-				path="/settings"
-				element={<Settings />}
-			/>
-			<Route
-				path="/generate"
-				element={<Generate />}
-			/>
-			<Route
-				path="/login"
-				element={<Login />}
-			/>
-		</Routes>
-	);
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	const [teste, setTeste] = useState(false);
+
+	switch (teste) {
+		case true:
+			return (
+				<Routes>
+					<Route
+						path="/"
+						element={<Home />}
+					/>
+					<Route
+						path="/profile"
+						element={<Profile />}
+					/>
+					<Route
+						path="/settings"
+						element={<Settings />}
+					/>
+					<Route
+						path="/generate"
+						element={<Generate />}
+					/>
+				</Routes>
+			);
+		default:
+			return (
+				<Routes>
+					<Route
+						path="/"
+						element={<Login />}
+					/>
+				</Routes>
+			);
+	}
 };
 export default Router;
