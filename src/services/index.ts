651ee7917835940112e3df79d2793b9af10142d2
malkavianson <63012624+Malkavianson@users.noreply.github.com>
2022-10-05 19:29:27 -0300

    fix: handle baseURL

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,4 +3,4 @@ import axios from "axios";
 export const api = axios.create({
 	baseURL: "https://vtmgenerator.herokuapp.com/",
 });
-// export const api = axios.create({ baseURL: process.env.REACT_APP_DATABASE_URL });
+// export const api = axios.create({ baseURL: "http://localhost:3333" });
